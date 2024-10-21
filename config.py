93ac809037e27eaa4da8b5ebbd3e76ed39d7af28
MANOJ AJJAKANA SHYAM <144515384+Manojajj@users.noreply.github.com>
2024-10-21 13:34:35 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///your_database.db')  # Replace with your DB URL
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
